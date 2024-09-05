3784d55fc023666b62a7a1f27f5375222961374c
yositasatiman <yosita_satiman@cmu.ac.th>
2024-09-05 17:00:24 +0700

    eiei

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,49 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Container, Text, Title, Rating, Textarea, Button, Divider, Pagination,Group, Center } from "@mantine/core";
+import { useState } from 'react';
+
+//const [activePage, setPage] = useState(1);
+
 
 export default function Home() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
-      <Text ta="center" my="sm">
-        Copyright © 2024 Chayanin Suatap 650610560
+      <Title order={4} mt = "sm"> Your rating </Title>
+      <Rating defaultValue={0} size="lg" />
+ 
+      <Textarea
+        size="sm" mt = "sm"
+        rows={3}
+        label = "Your review"
+        //description="Input description"
+        placeholder = "Do you enjoy eating?"
+      />
+
+      <Button variant="filled" color="orange" mt = "sm"> Submit Review </Button>
+  
+      <Divider my="md" />
+      <Group justify="center">
+      <Title order={4}> Elon Musk </Title>
+      <Rating defaultValue={5} size="sm" />
+      </Group>
+      <Text c="dimmed" size="sm" ta= "center">Best pizza in this world. l give you X score. </Text>
+      
+      <Divider my="md" />
+
+      <Group justify="center">
+      <Title order={4}> Mark Zuck </Title>
+      <Rating defaultValue={4} size="sm" />
+      </Group>
+      <Text c="dimmed" size="sm" ta= "center">My favourite part is pepperoni </Text>
+
+      <Group justify="center">
+      <Pagination color="orange" mt = "sm" boundaries ={1} total={20} />
+      </Group>
+
+      <Text ta="center" my="sm" c ="dimmed">
+        Copyright © 2024 Yosita Satiman 660610788
       </Text>
     </Container>
   );
